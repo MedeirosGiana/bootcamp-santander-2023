0e136d10974288705af87385a512889966bf1055
Giana Medeiros <gianamedeiros.00510@gmail.com>
2023-08-29 21:19:32 -0300

    update

--- OGrandeDeposito.java
+++ OGrandeDeposito.java
@@ -7,17 +7,12 @@ public class OGrandeDeposito {
         Scanner scanner = new Scanner(System.in);
 
         double valor = scanner.nextDouble();
-
-
+        
         if (valor > 0) {
-            //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
             System.out.printf("Deposito realizado com sucesso!\nSaldo atual: R$ %.2f",valor);
         } else if (valor == 0) {
-            //TODO: Imprimir a mensagem de valor inválido.
             System.out.println("Encerrando o programa...");
-
         } else {
-            //TODO: Imprimir a mensagem de encerrar o programa.
             System.out.println("Valor invalido! Digite um valor maior que zero.");
         }
         scanner.close();
